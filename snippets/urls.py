9e5424792f81af2e13dbd5119abe6f3762c4d1f1
Yaroslav Kupyak <kupjak@gmail.com>
2015-04-18 19:41:00 +0300

    Added post method - subscription, need to test

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,15 +7,16 @@ urlpatterns = patterns('',
 			url(r'^snippets/$', views.SnippetList.as_view()),
 			url(r'^user/$', views.UserAPI.as_view()),
 			url(r'^serials/$', views.SerialList.as_view()),
-			url(r'^snippets/po$', views.my_scheduled_job),
+			url(r'^subscribe/(?P<registrationId>[^/]+)$',views.SubscribeList.as_view()),
 			
 			url(r'^send$', views.my_scheduled_job),
 			
 # 			url(r'^snippets/$', views.get_list_of_serials),
  			url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view()),
-#  			url(r'^serials/(?P<pk>[0-9]+)$', views.SerialDetail.as_view()),
- 			url(r'^serials/(?P<pk>[0-9]+)&(?P<registrationId>[^/]+)$', views.SerialDetail.as_view(),name='detail'), #http://127.0.0.1:8000/serials/22?registrationId=1f1f4f
+                       #url(r'^serials/(?P<pk>[0-9]+)$', views.SerialDetail.as_view()),
+            url(r'^serials/(?P<pk>[0-9]+)&(?P<registrationId>[^/]+)$', views.SerialDetail.as_view(),name='detail'),
 #  			url(r'^serials/(?P<pk>)/Season/$', views.SerialDetail.as_view()),
+            
 			)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
